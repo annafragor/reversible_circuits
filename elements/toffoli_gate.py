34c9caf4e6aeb6dd7ab656ce82c9fe1559698db5
moskanka <moskanka@yandex.ru>
2020-03-24 17:54:35 +0300

    you can rotate toffoli gates and magnet them to lines

--- elements/toffoli_gate.py
+++ elements/toffoli_gate.py
@@ -2,17 +2,18 @@ from tkinter import *
 
 
 class ToffoliGate(Frame):
-	def __init__(self, canvas, n_controls, name, x=25, y=25, c=10):
+	def __init__(self, canvas, n_controls, name, x=25, y=25, c=10, up=True):
 		# n_controls - число управляющих линий
 		self.n_controls = n_controls
 		self.name_tag = name
 		self.center_point = (x, y)
 		self.c = c
+		self.up = up
 
 		self.canvas = canvas
-		self.create_gate(x, y)
+		self._create_gate(x, y)
 
-	def create_gate(self, x, y):
+	def _create_gate(self, x, y):
 		self.circle_id = self.canvas.create_oval(
 			(x - self.c, y - self.c), (x + self.c, y + self.c), 
 			outline="black", width=2, tags=(self.name_tag,)
@@ -20,16 +21,35 @@ class ToffoliGate(Frame):
 		self.horisontal_line_id = self.canvas.create_line(
 			(x - self.c, y), (x + self.c, y), width=2, tags=(self.name_tag,)
 		)
+        
+		if self.up:
+			ys_vertical = (
+				y - self.c, 
+				y + 2 * self.n_controls * self.c if self.n_controls > 0 else y + self.c
+ 			)
+		else:
+			ys_vertical = (
+				y - 2 * self.n_controls * self.c if self.n_controls > 0 else y - self.c,
+				y + self.c
+			)
 		self.vertical_line_id = self.canvas.create_line(
-			(x, y - self.c), (x, y + 2 * self.n_controls * self.c), 
+			(x, ys_vertical[0]), (x, ys_vertical[1]), 
 			width=2, tags=(self.name_tag,)
 		)
+
+		self.dots_id = []
 		for i in range(self.n_controls):
-			self.canvas.create_oval(
-				(x - 3, y + (i + 1) * 2 * self.c - 3), 
-				(x + 3, y + (i + 1) * 2 * self.c + 3), 
-				fill="black", tags=(self.name_tag,)
-			)
+			if self.up:
+				y_dot = y + (i + 1) * 2 * self.c
+			else:
+				y_dot = y - (i + 1) * 2 * self.c
+			self.dots_id.append(
+				self.canvas.create_oval(
+					(x - 3, y_dot - 3), 
+					(x + 3, y_dot + 3), 
+					fill="black", tags=(self.name_tag,)
+				)
+			)        
 
 	def set_central_point(self, **kwargs):
 		if 'x' in kwargs and 'y' in kwargs:
